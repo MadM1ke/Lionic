088248bdd6d2b52a930fc08c419a2b5a9a07ea2c
Mikhail <93826046+MadM1ke@users.noreply.github.com>
2022-09-26 12:55:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Бургер-меню
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".header__nav");
+let menuLinks = menu.querySelectorAll(".nav__link");
+
+burger.addEventListener(
+	"click",
+
+	function () {
+		burger.classList.toggle("burger--active");
+
+		menu.classList.toggle("header__nav--active");
+
+		document.body.classList.toggle("stop-scroll");
+	}
+);
+
+menuLinks.forEach(function (element) {
+	burger.classList.remove("burger--active");
+
+	menu.classList.remove("header__nav--active");
+
+	document.body.classList.remove("stop-scroll");
+});
+
+// Мобильная кнопка "Показать ещё"
+let articlesMore = document.querySelector(".articles-more");
+let articles = document.querySelectorAll(".articles__item");
+
+articlesMore.addEventListener("click", function () {
+	articles.forEach(function (el) {
+		el.classList.add("articles__item--visible");
+	});
+
+	articlesMore
+		.closest(".articles-center")
+		.classList.add("articles-center--hidden");
+});
